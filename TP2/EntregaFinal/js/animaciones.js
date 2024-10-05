a37d3017a90e0182e1e61457cef67bc903c77cef
Spherial <xsub123@gmail.com>
2024-10-04 23:47:23 -0300

    Refactor al codigo de animacion del carrusel. Comentado codigo

---
+++ TP2/EntregaFinal/js/animaciones.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", animar);
+
+
+function animar(){
+
+
+    //Me traigo todos los botones de los carruseles
+    let botones = document.querySelectorAll(".btn-carrusel");
+    console.log(botones);
+
+    let dir = 0;
+
+    //A cada boton, se le asigna un evento dependiendo si es para retrodecer o avanzar.
+    botones.forEach(boton =>{
+        console.log("Asignado evento al boton: " + boton);
+
+
+        //Segun si el boton es espejeado (izquierda) o no espejeado (derecha), defino la direccion
+        //hacia la que hay que animar
+        boton.addEventListener("click", ()=>{
+            let dir = boton.classList.contains("espejeado") ? 0 : 1;
+            animarCarrusel(boton, dir);
+        })
+
+
+
+
+    })
+
+
+
+    //Recibe el boton que lo llamó y la direccion a la que se animará
+    // 0 = izquierda
+    // 1 = derecha
+    function animarCarrusel(boton, direccion){
+        console.log(direccion);
+
+        //Consigo el abuelo del boton, // boton -> etiqueta a -> carrusel
+        let padre = boton.parentElement.parentElement;
+
+
+
+
+        //Segun la direccion, asigno la clase que anima al elemento, y a los 500ms la quito
+        if (direccion == 0){
+            padre.classList.add("carrusel-izq");
+            setTimeout(()=>{
+                padre.classList.remove("carrusel-izq");
+            }, 500)
+        }
+        else{
+            padre.classList.add("carrusel-der");
+            setTimeout(()=>{
+                padre.classList.remove("carrusel-der");
+            }, 500)
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
