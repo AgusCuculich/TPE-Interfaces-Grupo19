6c3d06dd09d063e96ff25a8751a16388ff030985
Spherial <xsub123@gmail.com>
2024-10-12 01:23:21 -0300

    Agregado loading simulado de 5 segundos al index

--- TP2/EntregaFinal/js/loading.js
+++ TP2/EntregaFinal/js/loading.js
@@ -15,6 +15,15 @@ function incrementarHasta100(porcentaje) {
     const interval = setInterval(() => {
         if (valor >= 100) {
             clearInterval(interval);
+            document.querySelector(".seccion-carga").classList.add("desaparecer");
+
+            setTimeout(()=>{
+                document.querySelector(".seccion-carga").remove();
+            },200);
+
+
+
+
         } else {
             valor += 0.1;
             porcentaje.textContent = valor.toFixed(0) + '%';
