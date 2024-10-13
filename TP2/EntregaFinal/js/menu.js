9d0d509750b1a621f240fcc3ceddef6c7414bdfd
Spherial <xsub123@gmail.com>
2024-10-13 18:56:28 -0300

    Funcionalidad para buscar juegos en mobile. Cambios en estilo de menu hamburguesa de mobile

--- TP2/EntregaFinal/js/menu.js
+++ TP2/EntregaFinal/js/menu.js
@@ -7,6 +7,16 @@ function control_menu(){
     let boton_menu = document.querySelector("#btn_hamburguesa");
     boton_menu.addEventListener("click", toggle_menu);
 
+    let boton_lupa = document.querySelector("#btn_search");
+    boton_lupa.addEventListener("click", ()=>{
+        toggle_menu();
+        //Focusear la barra de busqueda (solo si se llama desde la lupa)
+        document.querySelector(".search-mobile").focus();
+    });
+
+
+
+
     let boton_compartir = document.querySelector("#btn-compartir");
 
     if (boton_compartir){
@@ -30,4 +40,40 @@ function toggle_menu(){
 
     let icono_menu = document.querySelector("#btn_hamburguesa");
     icono_menu.classList.toggle("espejeado");
+
+
+    //Solo en mobile, al abrir el menu hamburguesa, se oscurece el resto de la pagina
+    if (!window.matchMedia("(min-width: 1024px)").matches) {
+        document.querySelector("main").classList.toggle("semitransparente");
+    }
+
+
+
+
+    //Se asegura que el oscurecimiento o aclaramiento se mantenga cuando el tamaño de la pantalla
+    //Cambie durante la ejecucion.
+    const mediaQuery = window.matchMedia("(min-width: 1024px)");
+    const chequearTamanioPantalla = (e) => {
+        let menu = document.querySelector(".menu_hamburguesa");
+
+        //Si es Desktop y el menu esta esconcido, se aclara
+        if (e.matches && !menu.classList.contains("oculto")) {
+            document.querySelector("main").classList.remove("semitransparente");
+        }
+
+        //Si es mobile y el menu esta presente, se oscurece
+        if (!e.matches && !menu.classList.contains("oculto")){
+            document.querySelector("main").classList.add("semitransparente");
+        }
+
+    };
+
+    mediaQuery.addEventListener("change", chequearTamanioPantalla);
+
+
+    // if (window.matchMedia("(min-width: 1024px)").matches){
+    //     document.querySelector("main").classList.remove("semitransparente");
+    // }
+
+
 }
